eb368140d695db1a7d0026848fb8c2bc333a678c
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2022-11-26 19:11:01 +0100

    Update presentation

--- assignment_4/solve_schrodinger.sh
+++ assignment_4/solve_schrodinger.sh
@@ -8,6 +8,12 @@
 for N in 100 1000 5000; do
     for xmax in 2.5 5 10 15; do
         echo "Running for N = $N, xmin = -$xmax, xmax = $xmax ..."
-        compiled/eigen_schrodinger --output_filename data/solution_$N\_$xmax.txt --npoints $N --xmin -$xmax --xmax $xmax
+        compiled/eigen_schrodinger \
+            --output_filename data/solution_$N\_$xmax.txt
+            --npoints $N
+            --xmin -$xmax
+            --xmax $xmax
     done
 done
+
+echo "Success!"
