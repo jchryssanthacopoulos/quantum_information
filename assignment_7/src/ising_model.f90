8f6ccd4cae1871e76d8e7fc704dc35779896bdec
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2022-12-18 12:04:45 +0100

    Fix implementation issues

--- assignment_7/src/ising_model.f90
+++ assignment_7/src/ising_model.f90
@@ -76,6 +76,8 @@ contains
 
         allocate(I(dim, dim))
 
+        I = (0d0, 0d0)
+
         do ii = 1, dim
             I(ii, ii) = (1d0, 0d0)
         end do
@@ -96,8 +98,9 @@ contains
         integer N, dim
         integer ii
         complex*16 sigma_z(2, 2)
-        complex*16, dimension(:, :), allocatable :: H_0
-        complex*16, dimension(:, :), allocatable :: H_0_i
+        complex*16, dimension(:, :), allocatable :: H_0, H_0_i
+
+        complex*16, dimension(:, :), allocatable :: I1, I2, prod1
 
         sigma_z = (0d0, 0d0)
         sigma_z(1, 1) = (1d0, 0d0)
@@ -106,13 +109,18 @@ contains
         dim = size(sigma_z, 1) ** N
 
         allocate(H_0(dim, dim))
-        allocate(H_0_i(dim, dim))
 
         H_0 = (0d0, 0d0)
 
         do ii = 1, N
-            H_0_i = tensor_product(tensor_product(identity(ii - 1), sigma_z), identity(N - ii))
+            I1 = identity(ii - 1)
+            I2 = identity(N - ii)
+            prod1 = tensor_product(I1, sigma_z)
+
+            H_0_i = tensor_product(prod1, I2)
             H_0 = H_0 + H_0_i
+
+            deallocate(I1, I2, prod1, H_0_i)
         end do
 
     end function
@@ -131,8 +139,9 @@ contains
         integer N, dim
         integer ii
         complex*16 sigma_x(2, 2)
-        complex*16, dimension(:, :), allocatable :: H_int
-        complex*16, dimension(:, :), allocatable :: H_int_i
+        complex*16, dimension(:, :), allocatable :: H_int, H_int_i
+
+        complex*16, dimension(:, :), allocatable :: I1, I2, prod1, prod2
 
         sigma_x = (0d0, 0d0)
         sigma_x(1, 2) = (1d0, 0d0)
@@ -141,13 +150,19 @@ contains
         dim = size(sigma_x, 1) ** N
 
         allocate(H_int(dim, dim))
-        allocate(H_int_i(dim, dim))
 
         H_int = (0d0, 0d0)
 
         do ii = 1, N - 1
-            H_int_i = tensor_product(tensor_product(tensor_product(identity(ii - 1), sigma_x) , sigma_x), identity(N - ii - 1))
+            I1 = identity(ii - 1)
+            I2 = identity(N - ii - 1)
+            prod1 = tensor_product(I1, sigma_x)
+            prod2 = tensor_product(prod1, sigma_x)
+
+            H_int_i = tensor_product(prod2, I2)
             H_int = H_int + H_int_i
+
+            deallocate(I1, I2, prod1, prod2, H_int_i)
         end do
 
     end function
@@ -164,7 +179,7 @@ contains
         complex*16, dimension(:, :) :: M
 
         do ii = 1, size(M, 1)
-            print '(*(sp, f7.4, 1x, f7.4, "i", 3x))', M(ii, :)
+            print '(*(sp, f8.5, 1x, f8.5, "i", 3x))', M(ii, :)
         end do
     end subroutine
 
@@ -190,14 +205,6 @@ program ising_model
     ! variables to clock time to diagonalize
     real*8 start, finish
 
-    ndim = 2 ** N
-
-    lwork = max(1, 2 * ndim - 1)
-
-    allocate(eigvals(ndim))
-    allocate(work(max(1, lwork)))
-    allocate(rwork(max(1, 3 * ndim - 2)))
-
     ! read number of subsystems and coupling constant
     call parse_cmd_args()
     write (arg_char, "(i8)") N
@@ -208,10 +215,19 @@ program ising_model
     print *, "debug = ", adjustl(arg_char)
     print *, "output_filename = ", output_filename
 
+    ndim = 2 ** N
+
+    lwork = max(1, 2 * ndim - 1)
+
+    allocate(eigvals(ndim))
+    allocate(work(max(1, lwork)))
+    allocate(rwork(max(1, 3 * ndim - 2)))
+
     ! compute Hamiltonian
     call cpu_time(start)
         H = lambda * non_interacting_hamiltonian(N)
         H = H + interacting_hamiltonian(N)
+    call cpu_time(finish)
     print "('Elapsed time to generate Hamiltonian = ', es16.10)", finish - start
 
     if (debug) then
@@ -224,6 +240,10 @@ program ising_model
         call zheev("N", "U", ndim, H, ndim, eigvals, work, lwork, rwork, info)
     call cpu_time(finish)
     print "('Elapsed time to diagonalize = ', es16.10)", finish - start
+
+    open(1, file=output_filename)
+    write(1, *) "Energy eigenvalues = "
+    write(1, *) eigvals
 
     deallocate(H, eigvals, work, rwork)
 
