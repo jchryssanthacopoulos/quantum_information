45efa033d5b6e6a1be0382b773399824f4f19dd7
James Chryssanthacopoulos <jchryssanthacopoulos@gmail.com>
2023-01-05 15:33:17 +0100

    Improve algorithm

--- assignment_8/src/dmrg_ising.f90
+++ assignment_8/src/dmrg_ising.f90
@@ -28,27 +28,30 @@ contains
     ! Inputs:
     !   N (integer): Number of sites in each bipartition
     !   H_1 (complex*16 matrix): Hamiltonian of each bipartition
-    !   H_12 (complex*16 matrix): Part of interaction Hamiltonian corresponding to left bipartition
-    !   H_34 (complex*16 matrix): Part of interaction Hamiltonian corresponding to right bipartition
+    !   A (complex*16 matrix): Part of interaction Hamiltonian corresponding to left bipartition
+    !   B (complex*16 matrix): Part of interaction Hamiltonian corresponding to right bipartition
+    !   lambda (real*8): Interaction with external magnetic field
     !   eigenvalues (real*8 array): Array to store energy eigenvalues
     !   diag_method (character): Method to use to diagonalize (i.e., dsyevr or zheev)
     !   debug (logical): Whether to display debug information
     !
     ! Returns:
-    !   Updates H_1, H_12, and H_34 in-place
+    !   Updates H_1, A, and B in-place
     !
-    subroutine iter_density_matrix_rg(N, H_1, A, B, eigenvalues, diag_method, debug)
+    subroutine iter_density_matrix_rg(N, H_1, A, B, lambda, eigenvalues, diag_method, debug)
         implicit none
 
         integer N, m, d, ndim, ii
         logical debug
         character(len=*) diag_method
 
+        real*8 lambda
+
         ! Pauli matrices
         complex*16 sigma_x(2, 2), sigma_z(2, 2)
 
         complex*16, dimension(:, :) :: H_1, A, B
-        complex*16, dimension(:, :), allocatable :: H_enlarged, H_12, H_23
+        complex*16, dimension(:, :), allocatable :: H_enlarged_1, H_enlarged_2, H_12, H_34, H_23
         complex*16, dimension(:, :), allocatable :: H
 
         ! eigenvalues and eigenvectors of H and rho
@@ -69,8 +72,10 @@ contains
         sigma_z = get_sigma_z()
 
         allocate(H(ndim ** 2, ndim ** 2))
-        allocate(H_enlarged(ndim, ndim))
+        allocate(H_enlarged_1(ndim, ndim))
+        allocate(H_enlarged_2(ndim, ndim))
         allocate(H_12(ndim, ndim))
+        allocate(H_34(ndim, ndim))
         allocate(H_23(ndim ** 2, ndim ** 2))
 
         allocate(eigenvalues_rho(ndim))
@@ -81,18 +86,23 @@ contains
 
         ! enlarge the block
         H_12 = tensor_product(A, sigma_x)
-        H_enlarged = tensor_product(H_1, identity(1)) + tensor_product(identity(N), sigma_z) + H_12
+        H_34 = tensor_product(sigma_x, B)
+        H_enlarged_1 = tensor_product(H_1, identity(1)) + lambda * tensor_product(identity(N), sigma_z) + H_12
+        H_enlarged_2 = tensor_product(identity(1), H_1) + lambda * tensor_product(sigma_z, identity(N)) + H_34
 
         ! interaction (always the same?)
         H_23 = tensor_product(tensor_product(identity(N), sigma_x), tensor_product(sigma_x, identity(N)))
 
         ! Hamiltonian of left and right enlarged blocks
-        H = tensor_product(H_enlarged, identity(N + 1)) + tensor_product(identity(N + 1), H_enlarged)
-        H = H + H_23
+        H = tensor_product(H_enlarged_1, identity(N + 1)) + tensor_product(identity(N + 1), H_enlarged_2) + H_23
 
         if (debug) then
             print *, "H_1 = "
             call print_complex_matrix(H_1)
+            print *, "A = "
+            call print_complex_matrix(A)
+            print *, "B = "
+            call print_complex_matrix(B)
             print *, "H_12 = "
             call print_complex_matrix(H_12)
             print *, "H_23 = "
@@ -104,18 +114,36 @@ contains
         ! diagonalize H
         call find_eigenvalues_using_zheev(H, eigenvalues, eigenvectors)
 
+        if (debug) then
+            print *, "eigenvalues = ", eigenvalues
+        end if
+
         ! compute density matrix of ground state
         rho = compute_density_matrix(eigenvectors(:, 1))
 
         ! compute left reduced density matrix
         rho_reduced_L = compute_left_reduced_density_matrix(rho, d, 2 * N + 2, N + 1)
 
+        if (debug) then
+            print *, "rho = "
+            call print_complex_matrix(rho)
+            print *, "rho_reduced_L = "
+            call print_complex_matrix(rho_reduced_L)
+        end if
+
         ! diagonalize reduced density matrix
         call find_eigenvalues_using_zheev(rho_reduced_L, eigenvalues_rho, eigenvectors_rho)
 
+        if (debug) then
+            print *, "eigenvalues of rho reduced L = ", eigenvalues_rho
+        end if
+
         ! truncate to get projection operator
         do ii = 1, m
-            P(:, ii) = eigenvectors_rho(:, ii)
+            if (debug) then
+                print *, "Using eigenvector with eigenvalue = ", eigenvalues_rho(ndim + 1 - ii)
+            end if
+            P(:, ii) = eigenvectors_rho(:, ndim + 1 - ii)
         end do
 
         if (debug) then
@@ -126,17 +154,22 @@ contains
         P_transpose = transpose(conjg(P))
 
         ! project into lower-dimensional subspace
-        H_1 = matmul(matmul(P_transpose, H_enlarged), P)
+        H_1 = matmul(matmul(P_transpose, H_enlarged_1), P)
         A = matmul(matmul(P_transpose, tensor_product(identity(1), A)), P)
+        A = B
+        ! B = matmul(matmul(P_transpose, tensor_product(B, identity(1))), P)
 
         if (debug) then
             print *, "H_1 after iteration = "
             call print_complex_matrix(H_1)
             print *, "A after iteration = "
             call print_complex_matrix(A)
+            print *, "B after iteration = "
+            call print_complex_matrix(B)
         end if
 
-        deallocate(H, H_enlarged, H_12, H_23, eigenvalues_rho, eigenvectors, eigenvectors_rho, rho, rho_reduced_L, P, P_transpose)
+        deallocate(H, H_enlarged_1, H_enlarged_2, H_12, H_34, H_23)
+        deallocate(eigenvalues_rho, eigenvectors, eigenvectors_rho, rho, rho_reduced_L, P, P_transpose)
 
     end subroutine
 
@@ -148,7 +181,7 @@ program dmrg_ising
     use density_matrix_rg
     implicit none
 
-    integer d, iter, npart
+    integer d, iter
 
     ! Pauli matrices
     complex*16 sigma_x(2, 2)
@@ -195,26 +228,19 @@ program dmrg_ising
     gs_energy = -1
     prev_gs_energy = 0
 
-    npart = N
-
     do while ((iter .le. max_iter) .and. abs(gs_energy - prev_gs_energy) > thres)
         prev_gs_energy = gs_energy
 
-        call iter_density_matrix_rg(N, H_1, A, B, eigenvalues, diag_method, debug)
-
-        npart = npart * 2
+        call iter_density_matrix_rg(N, H_1, A, B, lambda, eigenvalues, diag_method, debug)
 
         ! compute energy density
-        ! gs_energy = eigenvalues(1) / dble(N)
-        ! gs_energy = eigenvalues(1) / dble(2 * (N + 1 + iter))
-        gs_energy = eigenvalues(1) / dble(npart)
+        gs_energy = eigenvalues(1) / dble(d * (N + 1 + iter))
 
         if (debug) then
             print "('Iteration = ', i3, ', ground state energy = ', f9.4)", iter, gs_energy
         end if
 
         iter = iter + 1
-        npart = npart * 2
     end do
 
     ! perform final diagonalization to get energy of actual system
